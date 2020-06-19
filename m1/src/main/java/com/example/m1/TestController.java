62f24d01d6147a333c6b48a6790d6c6ad0412d95
wangchenxin <wangchenxin_0513@163.com>
2020-06-19 15:32:05 +0800

    初始化

---
+++ m1/src/main/java/com/example/m1/TestController.java
@@ -0,0 +1,17 @@
+package com.example.m1;
+
+import com.example.mapi.MDateUtil;
+
+/**
+ * @author wangchenxin
+ * @date 2020/6/19  10:57
+ * @desc
+ */
+
+
+public class TestController {
+    public static void main(String[] args) {
+        String s = MDateUtil.loadStrDate("",1);
+        System.out.println(s);
+    }
+}
---
+++ mapi/src/main/java/com/example/mapi/MDateUtil.java
@@ -0,0 +1,12 @@
+package com.example.mapi;
+
+/**
+ * @author wangchenxin
+ * @date 2020/6/19  10:56
+ * @desc
+ */
+public class MDateUtil {
+    public static String loadStrDate(String i,int a){
+        return "00000000";
+    }
+}
