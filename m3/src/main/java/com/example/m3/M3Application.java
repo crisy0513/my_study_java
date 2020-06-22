232cd7a366c9e1889c635095fda79709c78f5442
wangchenxin <wangchenxin_0513@163.com>
2020-06-22 14:35:28 +0800

    config

--- m3/src/main/java/com/example/m3/M3Application.java
+++ m3/src/main/java/com/example/m3/M3Application.java
@@ -2,7 +2,9 @@ package com.example.m3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -11,6 +13,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient//启用服务注册与发现
 @EnableFeignClients//启用feign进行远程调用
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class M3Application {
 
     public static void main(String[] args) {
--- m3/src/main/java/com/example/m3/Remote.java
+++ m3/src/main/java/com/example/m3/Remote.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @date 2020/6/22  11:41
  * @desc
  */
-@FeignClient(name= "spring-cloud-producer")
+@FeignClient(name= "spring-cloud-producer",fallback = RemoteHystrix.class)
 public interface Remote {
     @RequestMapping(value = "/hello")
     public String hello(@RequestParam(value = "name") String name);
