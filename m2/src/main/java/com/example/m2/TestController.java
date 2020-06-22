50c59b0e4a3c6c46d9b8806db0ef212204751c3e
wangchenxin <wangchenxin_0513@163.com>
2020-06-22 11:18:59 +0800

    wcx提交

---
+++ m2/src/main/java/com/example/m2/TestController.java
@@ -0,0 +1,12 @@
+package com.example.m2;
+
+/**
+ * @author wangchenxin
+ * @date 2020/6/22  11:18
+ * @desc
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("wcx");
+    }
+}
